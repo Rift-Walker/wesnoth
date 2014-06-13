82d3aa51e0487bc588670e48488219f2dbc65bca
Nathan Walker <nathan.b.walker@vanderbilt.edu>
2014-06-13 12:33:19 -0500

    Add define functionality to modifications, fix reloading for mp campaigns.
    Added a new method, void create_engine::prepare_for_era_and_mods(),
    which sets era and mod in game classification.
    Prepare_for_[scenario, campaign] then set their respective defines and reload.

--- src/multiplayer_create_engine.cpp
+++ src/multiplayer_create_engine.cpp
@@ -414,15 +414,24 @@ void create_engine::prepare_for_new_level()
 	parameters_.mp_scenario_name = parameters_.scenario_data["name"].str();
 }
 
-void create_engine::prepare_for_scenario()
+void create_engine::prepare_for_era_and_mods()
 {
-	DBG_MP << "preparing data for campaign by reloading game config\n";
-
-	state_.classification().scenario_define =
-		current_level().data()["define"].str();
 	state_.classification().era_define =
 		resources::config_manager->game_config().find_child(
 			"era", "id", get_parameters().mp_era)["define"].str();
+	BOOST_FOREACH(const std::string& mod_id, get_parameters().active_mods) {
+		state_.classification().mod_defines.push_back(
+				resources::config_manager->game_config().find_child(
+					"modification", "id", mod_id)["define"].str());
+	}
+}
+
+void create_engine::prepare_for_scenario()
+{
+	DBG_MP << "preparing data for scenario by reloading game config\n";
+
+	state_.classification().scenario_define =
+		current_level().data()["define"].str();
 	
 	resources::config_manager->
 		load_game_config_for_game(state_.classification());
